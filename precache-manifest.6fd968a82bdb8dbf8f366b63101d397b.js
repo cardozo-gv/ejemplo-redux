5c6a92d2e1d35e9719582de2060a100d8514b96e
Your Name <griseldavcardozo@gmail.com>
2019-07-12 14:58:24 -0300

    Updates

---
+++ precache-manifest.6fd968a82bdb8dbf8f366b63101d397b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e2e61553e6f02e9ae1cbf5718d5119f",
+    "url": "/ejemplo-redux/index.html"
+  },
+  {
+    "revision": "0a93e4f8018f70b81b54",
+    "url": "/ejemplo-redux/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e912c566917027ee5a54",
+    "url": "/ejemplo-redux/static/js/2.4d00ac66.chunk.js"
+  },
+  {
+    "revision": "0a93e4f8018f70b81b54",
+    "url": "/ejemplo-redux/static/js/main.22fe9b73.chunk.js"
+  },
+  {
+    "revision": "4c6b181e1e9a535df8c1",
+    "url": "/ejemplo-redux/static/js/runtime~main.651af93e.js"
+  }
+]);
