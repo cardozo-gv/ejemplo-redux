ba1e23af284045c8b75df190a95eb44d19db6435
Your Name <griseldavcardozo@gmail.com>
2019-07-13 18:00:40 -0300

    combineReducers

--- src/actionsCreators.js
+++ src/actionsCreators.js
@@ -8,6 +8,19 @@
    *Permiten agregar logica adicional
 */
 
+
+
+const loadProducts = () => {
+  return {
+    type : "REPLACE_PRODUCTS",
+    products : [
+      { id: 1, name: "Hipster Ultimate", price: 299, image: "https://s3.amazonaws.com/makeitreal/projects/e-commerce/camiseta-1.jpg" },
+      { id: 2, name: "On Motion Live", price: 99, image: "https://s3.amazonaws.com/makeitreal/projects/e-commerce/camiseta-2.jpg" },
+      { id: 3, name: "Underground Max", price: 149, image: "https://s3.amazonaws.com/makeitreal/projects/e-commerce/camiseta-3.jpg" },
+    ]
+  }
+}
+
 const addToCart = product => {
   return {
     type:"ADD_TO_CART",
@@ -22,4 +35,4 @@ const removeFromCart = product => {
   }
 }
 
-export {addToCart, removeFromCart}
+export {addToCart, removeFromCart, loadProducts}
--- src/store.js
+++ src/store.js
@@ -1,36 +1,40 @@
 /* Mantiene el estado de la aplicacion de forma centralizada
-  * permite el acceso al estado a traves del getState()
-  * Registra los suscriptores a traves de subscribe(fn).
-  * Permite que el estado sea actualizado a traves del metodo dispatch(action)
-  * El store se crea con el metodo createStore(reduceFn,estado inicial)
-  ---------------
-  La funcion reductora es una funcion que recibe un estado, una accion y retorna
-  un nuevo estado
-  El store invoca la funcion reductora siempre que recibe un llamado al metodo dispatch(action)
+* permite el acceso al estado a traves del getState()
+* Registra los suscriptores a traves de subscribe(fn).
+* Permite que el estado sea actualizado a traves del metodo dispatch(action)
+* El store se crea con el metodo createStore(reduceFn,estado inicial)
+---------------
+La funcion reductora es una funcion que recibe un estado, una accion y retorna
+un nuevo estado
+El store invoca la funcion reductora siempre que recibe un llamado al metodo dispatch(action)
 */
 
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 
-const reducer = (state,action) => {
-  if(action.type === "ADD_TO_CART"){
-    return {
-      ...state,
-      cart : state.cart.concat(action.product)
-    }
-  };
-  if(action.type === "REMOVE_FROM_CART"){
-    return {
-      ...state,
-      cart : state.cart.filter(e => e.id !== action.product.id),
-    }
+/*El combineReducers divide el reducers segun su funcionalidad
+  ahora cada función reductora NO retorna todo el estado. Solamente retorna
+  la parte del estado que importa
+ */
+
+const products = (state = [],action) => {
+  if( action.type === "REPLACE_PRODUCTS"){
+    return action.products
   }
   return state;
 }
 
-const products = [
-  { id: 1, name: "Hipster Ultimate", price: 299, image: "https://s3.amazonaws.com/makeitreal/projects/e-commerce/camiseta-1.jpg" },
-  { id: 2, name: "On Motion Live", price: 99, image: "https://s3.amazonaws.com/makeitreal/projects/e-commerce/camiseta-2.jpg" },
-  { id: 3, name: "Underground Max", price: 149, image: "https://s3.amazonaws.com/makeitreal/projects/e-commerce/camiseta-3.jpg" },
-]
+const cart = (state = [],action) => {
+  if(action.type === "ADD_TO_CART"){
+    return state.concat(action.product)
+  }
+  else if(action.type === "REMOVE_FROM_CART"){
+    return state.filter(e => e.id !== action.product.id)
+  }
+  return state;
+}
 
-export default createStore(reducer,{cart:[],products:products});
+
+/* la llave cart va a ejecutar la funcion car, la llave products va a ejecutar la funcion products
+como tienen el  mismo nombre llave y funcion se puede resumir así
+combineReducers({cart,products}) */
+export default createStore(combineReducers({cart:cart, products:products}));
